a851a8652b4e5ca873e2bb0132aecbc806639804
Nathan Steed <steedn@outlook.com>
2020-08-05 03:12:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,27 +2,9 @@ class Book
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end
-  def author=(author)
-    @author = author
-  end
-  def author
-    @author
-  end
-  def page_count=(num)
-    @page_count = num
-  end
-  def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre
-  end
-  def genre
-    @genre
-  end
+
+  attr_accessor :author, :page_count, :genre
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
